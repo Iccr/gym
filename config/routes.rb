7a5c3c34cc2277ab9f08abf57c4c9d5bf0a6e493
ccr <sis.ccr@gmail.com>
2017-05-24 21:54:36 +0545

    exercises controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'exercises/index'
+
+  get 'exercises/create'
+
+  get 'exercises/update'
+
+  get 'exercises/show'
+
+  get 'exercises/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
