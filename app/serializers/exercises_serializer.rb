a77b4edd8461be90b040a7d2e5a1c06a91bff1cc
ccr <sis.ccr@gmail.com>
2017-05-28 22:16:00 +0545

    attribute added in serializer

--- app/serializers/exercises_serializer.rb
+++ app/serializers/exercises_serializer.rb
@@ -1,3 +1,11 @@
 class ExercisesSerializer < ActiveModel::Serializer
-  attributes :id
+  # Table name: exercises
+  #
+  #  id         :integer          not null, primary key
+  #  name       :string
+  #  created_at :datetime         not null
+  #  updated_at :datetime         not null
+  #
+
+  attributes :id, :name
 end
